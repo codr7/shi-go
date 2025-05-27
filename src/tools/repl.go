0f8b61c2ae11711f8f0a5005fc159ac0c767720a
Andreas Nilsson <codr7@protonmail.com>
2025-05-27 04:57:30 +0200

    Improve repl.

--- src/tools/repl.go
+++ src/tools/repl.go
@@ -14,6 +14,7 @@ func Repl(vm *shi.VM) {
 	in := bufio.NewScanner(os.Stdin)
 	out := bufio.NewWriter(os.Stdout)
 	var b bytes.Buffer
+	sloc := shi.NewSloc("repl")
 
 	for {
 		if wd, err := os.Getwd(); err != nil {
@@ -23,45 +24,40 @@ func Repl(vm *shi.VM) {
 			fmt.Fprintf(out, "%v$ ", wd)
 			out.Flush()
 		}
-				
+
+		MORE:		
+
 		if !in.Scan() {
 			if err := in.Err(); err != nil {
 				log.Fatal(err)
+				break
 			}
-
-			break
 		}
 
-		line := in.Text()
-
-		if line == "" {
-			sloc := shi.NewSloc("repl")
-			pc := vm.EmitPC()
-			var fs shi.Deque[shi.Form]
-
-			if err := vm.ReadAll(bufio.NewReader(&b), &fs, sloc);
-			err != nil {
-				fmt.Println(err)
-				b.Reset()
-				goto NEXT
-			}
-
-			b.Reset()
-
-			if err := forms.EmitAll(&fs, vm); err != nil {
-				fmt.Println(err)
-				goto NEXT
-			}
-
-			if err := vm.Eval(pc, -1); err != nil {
-				fmt.Println(err)
-				goto NEXT
-			}
-		NEXT:
-			shi.DumpStack(vm.Stack, out, vm)
-			out.WriteRune('\n')
-		} else {
-			fmt.Fprintln(&b, line)
+		fmt.Fprintln(&b, in.Text())
+		pc := vm.EmitPC()
+		var fs shi.Deque[shi.Form]
+		rb := b
+		
+		if err := vm.ReadAll(bufio.NewReader(&rb), &fs, sloc);
+		err != nil {
+			goto MORE
 		}
+		
+		b.Reset()
+
+		if err := forms.EmitAll(&fs, vm); err != nil {
+			fmt.Println(err)
+			goto DONE
+		}
+
+		if err := vm.Eval(pc, -1); err != nil {
+			fmt.Println(err)
+			goto DONE
+		}
+
+	DONE:
+		shi.DumpStack(vm.Stack, out, vm)
+		out.WriteRune('\n')
 	}
 }
