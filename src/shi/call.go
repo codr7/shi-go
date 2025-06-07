8a45d3e856f3dfe87bde06e5c925fa26dbac8737
codr7 <codr7@protonmail.com>
2025-06-07 18:15:22 +0200

    Extract term.

--- src/shi/call.go
+++
@@ -1,7 +0,0 @@
-package shi
-
-type Call struct {
-	returnPC PC
-	sloc Sloc
-	target Method
-}
--- src/tools/repl.go
+++ src/tools/repl.go
@@ -3,80 +3,59 @@ package tools
 import (
 	"bufio"
 	"bytes"
-	//"fmt"
+	"fmt"
+	"log"
 	"os"
 	"shi/src/shi"
 	"shi/src/forms"
 )
 
 func Repl(vm *shi.VM) {
-	var t Term
-	t.Init(os.Stdin, os.Stdout)
-	defer t.Restore()
-
+	in := bufio.NewScanner(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
 	var code bytes.Buffer
 	sloc := shi.NewSloc("repl")
 	var stack shi.Values
 	
 	for {
-		t.Printf("%2v ", sloc.Line()).Flush()
-		var lb bytes.Buffer
-		i := 1
+		fmt.Fprintf(out, "%2v ", sloc.Line())
+		out.Flush()
 		
-		for {
-			in, _ := t.GetChar()
-
-			if len(in) == 1 {
-				if c := in[0]; c == ENTER {
-					if lb.Len() == 0 {
-						break
-					} else {
-						code.WriteString(lb.String())
-						code.WriteRune('\n')
-						lb.Reset()
-						t.Br().Printf("%2v ", sloc.Line()+i).Flush()
-						i++
-					}
-				} else if c == BACKSPACE {
-					if n := lb.Len(); n > 0 {
-						lb.Truncate(n-1)
-						t.Backspace().Flush()
-					}
-				} else if c == CTRL_D {
-					return
-				} else {
-					lb.WriteRune(c)
-					t.Buffer.WriteRune(c)
-					t.Flush()
-				}
-			} else {
-				panic("Not implemented")
+		if !in.Scan() {
+			if err := in.Err(); err != nil {
+				log.Fatal(err)
 			}
+
+			break
 		}
+
+		line := in.Text()
 		
-		pc := vm.EmitPC()
-		var fs shi.Forms
-		
-		if err := vm.ReadAll(bufio.NewReader(&code), &fs, sloc);
-		err != nil {
-			t.Br().Println(err).Flush()
-			code.Reset()
-			goto DONE
+		if line == "" {
+			pc := vm.EmitPC()
+			var fs shi.Forms
+			
+			if err := vm.ReadAll(bufio.NewReader(&code), &fs, sloc);
+			err != nil {
+				fmt.Fprintln(out, err)
+				code.Reset()
+				goto NEXT
+			}
+			
+			if err := forms.EmitAll(&fs, vm); err != nil {
+				fmt.Fprintln(out, err)
+				goto NEXT
+			}
+			
+			if err := vm.Eval(pc, -1, &stack); err != nil {
+				fmt.Fprintln(out, err)
+			}
+		NEXT:
+			shi.DumpStack(stack, out, vm)
+			out.WriteString("\n\n")
+			out.Flush()
+		} else {
+			fmt.Fprintln(&code, line)
 		}
-		
-		if err := forms.EmitAll(&fs, vm); err != nil {
-			t.Br().Println(err).Flush()
-			goto DONE
-		}
-		
-		if err := vm.Eval(pc, -1, &stack); err != nil {
-			t.Br().Println(err).Flush()
-			goto DONE
-		}
-		
-	DONE:
-		t.Br()
-		shi.DumpStack(stack, &t.Buffer, vm)
-		t.Br().Br().Flush()
 	}
 }
